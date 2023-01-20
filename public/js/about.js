658823cd8fc4f6892e4871755b75e3f5eca5d0f8
Bailey Mejia <baileymejia28@gmail.com>
2023-01-19 23:54:42 -0800

    hidden project cards for mobile, fixed the span elements for the title, and added carousel.

--- public/js/about.js
+++ public/js/about.js
@@ -1,6 +1,25 @@
+//Define burger button
 const aboutBurgerBtn = document.getElementById('aboutBtn');
 const aboutMenu = document.getElementById('mobile-menu');
 
+//Define carousel buttons
+const prevButton = document.querySelectorAll('.carousel-control-prev');
+const nextButton = document.querySelectorAll('.carousel-control-next');
+
+//Define carousel item
+const carouselItem = document.querySelectorAll('.carousel-item');
+
+
+//create an index that is 0
+//The index has to start at 0 so it can keep building up
+let index = 0;
+
+//Create addEventListener
+prevButton.addEventListener('click', () => {
+    // I LEFT OFF HERE DONT FORGET
+})
+
+//hide/unhide burgerbtn
 aboutBurgerBtn.addEventListener('click', () => {
     if (aboutMenu.style.display === 'none') {
         aboutMenu.style.display = 'block';
@@ -9,3 +28,5 @@ aboutBurgerBtn.addEventListener('click', () => {
     }
     console.log('Hello')
 });
+
+
