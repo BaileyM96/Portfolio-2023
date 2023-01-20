6c87570724857e0c9c13e684fdacb5762be6b74c
Bailey Mejia <baileymejia28@gmail.com>
2023-01-20 15:58:50 -0800

    Added projects section to desktop version.

--- public/js/about.js
+++ public/js/about.js
@@ -3,21 +3,15 @@ const aboutBurgerBtn = document.getElementById('aboutBtn');
 const aboutMenu = document.getElementById('mobile-menu');
 
 //Define carousel buttons
-const prevButton = document.querySelectorAll('.carousel-control-prev');
-const nextButton = document.querySelectorAll('.carousel-control-next');
+const prevButton = document.querySelector('.carousel-control-prev');
+const nextButton = document.querySelector('.carousel-control-next');
 
 //Define carousel item
 const carouselItem = document.querySelectorAll('.carousel-item');
 
+//iterate over images
+let carouselIndex = 0;
 
-//create an index that is 0
-//The index has to start at 0 so it can keep building up
-let index = 0;
-
-//Create addEventListener
-prevButton.addEventListener('click', () => {
-    // I LEFT OFF HERE DONT FORGET
-})
 
 //hide/unhide burgerbtn
 aboutBurgerBtn.addEventListener('click', () => {
@@ -28,5 +22,23 @@ aboutBurgerBtn.addEventListener('click', () => {
     }
     console.log('Hello')
 });
+
+// //Toggle previous photo
+// prevButton.addEventListener('click', () => {
+//     carouselItem[carouselIndex].classList.remove('active');
+//     //show previous image with ternary operator
+//     carouselIndex = carouselIndex > 0 ? carouselIndex -1 : carouselItem.length - 1;
+//     carouselItem[carouselIndex].classList.add('active');
+//     console.log('click previous');
+// });
+
+// // //Next button
+// nextButton.addEventListener('click', () => {
+//     carouselItem[carouselIndex].classList.remove('active');
+//     //show next image with ternary operator
+//     carouselIndex = carouselIndex < carouselItem.length -1 ? carouselIndex + 1 : 0;
+//     carouselItem[carouselIndex].classList.add('active');
+//     console.log('click next');
+// });
 
 
